e7bd704b58181d1d7f0be7fdc1119ac0668b10cc
Matias Pierri <matias.p@marvik.ai>
2023-05-14 18:28:16 -0300

    Spring boot initializer

---
+++ tp/src/test/java/com/programacionIII/tp/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacionIII.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
