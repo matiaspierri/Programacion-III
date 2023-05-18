2c074fe3c7e2a72bc086dd2ca891f9f60b1c95b0
Matias Pierri <matias.p@marvik.ai>
2023-05-18 02:13:07 -0300

    repo

--- tp/src/test/java/com/programacionIII/tp/TpApplicationTests.java
+++ tp/src/test/java/com/programacionIII/tp/TpApplicationTests.java
@@ -2,23 +2,28 @@ package com.programacionIII.tp;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.programacionIII.tp.services.IUserService;
 import com.programacionIII.tp.models.User;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
 
 @SpringBootTest
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TpApplicationTests {
 
     @Autowired
     private IUserService userService;
 
-    @BeforeAll
-    void beforeAll() {
-        userService.createUser("Juan", "Perez", "juanperez", "12345678", null);
-    }
-
+//    @BeforeAll
+//    void beforeAll() {
+//        userService.createUser("Juan", "Perez", "juanperez", "12345678", null);
+//    }
+//
     @Test
     void contextLoads() {
         //userService.createUser("Juan", "Perez", "juanperez", "12345678", null);
