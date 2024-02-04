7c49d9af8dece2f834fc11ab313a7019954a9ce1
Matias Pierri <matias.p@marvik.ai>
2024-02-04 16:21:00 -0300

    init spring

---
+++ programacion-3/src/main/java/com/example/programacion3/Programacion3Application.java
@@ -0,0 +1,13 @@
+package com.example.programacion3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programacion3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Programacion3Application.class, args);
+	}
+
+}
