d0ed2598e9a8c13118c67612e7dd1db741b844d9
Matias Pierri <matias.p@marvik.ai>
2024-02-15 23:22:41 -0300

    Remove all version

--- tp/src/main/java/com/programacionIII/tp/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.programacionIII.tp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.programacionIII.tp.models.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-
-}
